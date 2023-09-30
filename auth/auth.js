87c64470f0fa093e4a2838371c7fcd42e524c140
Mika Schmitt <mika.schmitt02@gmail.com>
2023-09-30 17:19:37 +0200

    Authentication working

---
+++ auth/auth.js
@@ -0,0 +1,28 @@
+var auth_url = 'https://accounts.google.com/o/oauth2/auth?';
+var client_id = '35905430505-4t6dea16au28vjtu3lac1dtj906kna5g.apps.googleusercontent.com';  // must be Web Application type
+var redirect_url = chrome.identity.getRedirectURL(); // make sure to define Authorised redirect URIs in the Google Console such as https://<-your-extension-ID->.chromiumapp.org/
+
+var auth_params = {
+    client_id: client_id,
+    redirect_uri: redirect_url,
+    response_type: 'token',
+    scope: 'https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar.readonly'
+};
+
+const url = new URLSearchParams(Object.entries(auth_params));
+url.toString();
+auth_url += url;
+
+chrome.identity.launchWebAuthFlow({url: auth_url, interactive: true}, function(responseUrl) { 
+    console.log(responseUrl);
+    var url;
+    try {
+        url = new URL(responseUrl);
+    } catch (error) {
+        console.error(error);
+    }         
+    var token = new URLSearchParams(url.hash.substring(1)).get('access_token');
+    chrome.storage.local.set({ "authToken": token }, function(){
+        console.log(`aquired and stored token: ${token}`);
+    });
+});
---
+++ background.js
@@ -0,0 +1,6 @@
+chrome.windows.create({
+    'url': './auth/auth.html',
+    'width': 454,
+    'height': 540,
+    'type': 'popup'
+});
